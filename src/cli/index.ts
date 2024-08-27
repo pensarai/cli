283783c00d95ef571ced02f9cb0bcc6047bb6302
Kerem Proulx <kerem@pensarai.com>
2024-08-27 20:55:55 +0000

    cleanups

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import { scanCommandHandler } from "./commands/scan";
 
-async function main() {
+export async function initCli() {
 
     const program = new Command();
     
@@ -9,11 +9,9 @@ async function main() {
     .description("Scan for vulnerabilties starting in a target location. This command will recursively scan files starting from the target location.")
     .option("-lang, --language <language>", "Target a specific language/filetype")
     .option("--local", "Run local LLM inference")
-    .option("--api_key", "Specify OpenAI API key. This will default to `proccess.env.OPENAI_API_KET`")
+    .option("--api_key", "Specify OpenAI API key. This will default to `proccess.env.OPENAI_API_KEY`")
     .option("--github", "Specify if the CLI is being run in a github action. This will use generated fixes to create PRs instead of presenting the user the ability to locally apply patches.")
     .action(scanCommandHandler);
 
     await program.parseAsync(process.argv);
 }
-
-main();
