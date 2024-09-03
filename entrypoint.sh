589001d6bd6d6aeca282fee3bcaa96bb30dcdec3
Kerem Proulx <kerpro26@gmail.com>
2024-09-03 19:14:58 -0400

    disable logging metrics to the console by default in action.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 if ["$2" = "true"]; then
-    pensar scan -lang $1 --github --local
+    pensar scan -lang $1 --github --local --no_metrics
 else
-    pensar scan -lang $1 --github
+    pensar scan -lang $1 --github --no_metrics
 fi
