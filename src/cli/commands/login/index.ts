a2c94a20b692a9b5a76aca10ad4404b7d50f2551
KeremP <kerpro26@gmail.com>
2024-09-16 15:15:37 +0000

    add api url env var detection

--- src/cli/commands/login/index.ts
+++ src/cli/commands/login/index.ts
@@ -1,4 +1,6 @@
-const ConsoleUrl = "https://cosole.pensar.dev/login";
+import { config } from "@/lib/config";
+
+const ConsoleUrl = `${config.CONSOLE_URL}/login`;
 
 export function loginCommandHandler() {
     console.log(`Opening browser window at ${ConsoleUrl}.\n\nPlease follow the steps to login there and create an API key.`);
