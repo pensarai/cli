41e624dd29e320182732829d2804848e1312153d
KeremP <kerpro26@gmail.com>
2024-09-19 15:32:04 +0000

    update supported set of langauges

--- src/cli/commands/scan/utils.ts
+++ src/cli/commands/scan/utils.ts
@@ -2,15 +2,27 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { type Language } from '@pensar/semgrep-node';
 
-// TODO: reduce this to the set of languages supported by semgrep-core
-// const SUPPORTED_LANGUAGES: Set<string> = new Set([
-//   "ts", "js", "py", "go", "cpp", "c", "java", "rb", "php", "cs", "scala", "kt", "swift",
-//   "rust", "hs", "ml", "clj", "ex", "lua", "pl", "sh", "sql", "r", "m"
-// ]); 
-
-const SUPPORTED_LANGUAGES: Set<string> = new Set([
-    "ts", "js", "go", "cpp"
+const SUPPORTED_LANGUAGES = new Set([
+  'apex', 'bash', 'c', 'c#', 'c++', 'cairo', 'circom', 'clojure', 'cpp', 'csharp',
+  'dart', 'docker', 'dockerfile', 'elixir', 'ex', 'generic', 'go', 'golang', 'hack',
+  'hcl', 'html', 'java', 'javascript', 'js', 'json', 'jsonnet', 'julia', 'kotlin',
+  'kt', 'lisp', 'lua', 'move_on_aptos', 'none', 'ocaml', 'php', 'promql', 'proto',
+  'proto3', 'protobuf', 'py', 'python', 'python2', 'python3', 'ql', 'r', 'regex',
+  'ruby', 'rust', 'scala', 'scheme', 'sh', 'sol', 'solidity', 'swift', 'terraform',
+  'tf', 'ts', 'typescript', 'vue', 'xml', 'yaml'
 ]);
+
+const EXTENSION_TO_LANGUAGE: { [key: string]: string } = {
+  'js': 'javascript',
+  'py': 'python',
+  'ts': 'typescript',
+  'cpp': 'c++',
+  'cs': 'c#',
+  'kt': 'kotlin',
+  'ex': 'elixir',
+  'tf': 'terraform',
+  'sol': 'solidity',
+};
 
 export function detectProgrammingLanguages(directory: string = '.'): Set<string> {
   const detectedLanguages = new Set<string>();
@@ -26,8 +38,20 @@ export function detectProgrammingLanguages(directory: string = '.'): Set<string>
         traverse(filePath);
       } else if (stat.isFile()) {
         const extension = path.extname(file).slice(1).toLowerCase();
+        const fileName = path.basename(file).toLowerCase();
+
+        // Check if the file extension or name matches a supported language
         if (SUPPORTED_LANGUAGES.has(extension)) {
-          detectedLanguages.add(extension);
+          detectedLanguages.add(EXTENSION_TO_LANGUAGE[extension] || extension);
+        } else if (SUPPORTED_LANGUAGES.has(fileName)) {
+          detectedLanguages.add(fileName);
+        }
+
+        // Special cases
+        if (fileName === 'dockerfile') {
+          detectedLanguages.add('docker');
+        } else if (extension === 'sh' || fileName.endsWith('.sh')) {
+          detectedLanguages.add('bash');
         }
       }
     }
